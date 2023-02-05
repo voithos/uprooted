9a3121d9193ea133c3ceaf95156a80d4ccbcb4d0
Zaven Muradyan <zaven@voithos.io>
2023-02-05 09:38:03 -0800

    Implement basic grass.

---
+++ scenes/GrassMesh.gd
@@ -0,0 +1,51 @@
+tool
+extends MultiMeshInstance
+
+export var extents := Vector2.ONE
+export var spawn_outside_circle := false
+export var radius := 12.0
+export var character_path := NodePath()
+
+var _character: Spatial
+
+func _enter_tree() -> void:
+    connect("visibility_changed", self, "_on_WindGrass_visibility_changed")
+
+
+func _ready() -> void:
+    if character_path:
+        _character = get_node(character_path)
+    var rng := RandomNumberGenerator.new()
+    rng.randomize()
+
+    var theta := 0
+    var increase := 1
+    var center: Vector3 = get_parent().global_transform.origin
+
+    for instance_index in multimesh.instance_count:
+        var transform := Transform().rotated(Vector3.UP, rng.randf_range(-PI / 2, PI / 2))
+        var x: float
+        var z: float
+        if spawn_outside_circle:
+            x = center.x + (radius + rng.randf_range(0, extents.x)) * cos(theta)
+            z = center.z + (radius + rng.randf_range(0, extents.y)) * sin(theta)
+            theta += increase
+        else:
+            x = rng.randf_range(-extents.x, extents.x)
+            z = rng.randf_range(-extents.y, extents.y)
+            
+        transform.origin = Vector3(x, 0, z)
+
+        multimesh.set_instance_transform(instance_index, transform)
+
+
+func _on_WindGrass_visibility_changed() -> void:
+    if visible:
+        _ready()
+
+
+func _process(_delta: float) -> void:
+    if _character:
+        material_override.set_shader_param(
+            "character_position", _character.global_transform.origin
+        )
