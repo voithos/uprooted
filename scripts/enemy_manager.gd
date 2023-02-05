0a6cbcdda8bc391edf754ee59ca105265164ec21
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2023-02-05 15:26:33 -0800

    Update spawn rates.

--- scripts/enemy_manager.gd
+++ scripts/enemy_manager.gd
@@ -20,7 +20,6 @@ const STARTING_WAVE_INDEX_FOR_DEBUGGING := 0
 const EXPLICIT_WAVE_CONFIGS := [
     { small = 1, medium = 4, large = 0 },
     { small = 3, medium = 0, large = 0 },
-    { small = 3, medium = 0, large = 0 },
     { small = 3, medium = 1, large = 0 },
     { small = 3, medium = 0, large = 0 },
     { small = 3, medium = 1, large = 0 }, # Wave 5
