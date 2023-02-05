81e3110b42c39259170b84b9561f5d5549ac1383
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2023-02-05 01:34:00 -0800

    Add funny game-over messages.

--- scenes/gui/GameOverScreen.gd
+++ scenes/gui/GameOverScreen.gd
@@ -5,9 +5,16 @@ extends ScreenPanel
 
 const MARGIN_RATIO := 0.2
 
+const GAME_OVER_MESSAGES := [
+    "You got chopped!",
+    "Splintered!",
+    "Turned into toothpicks!",
+]
+
 
 func set_is_open(value: bool, fades := true) -> void:
     .set_is_open(value, fades)
+    get_node("%Header").text = _get_game_over_message()
     get_node("%Score").text = str(Session.score)
     get_node("%WaveCount").text = str(Session.waves_completed)
 
@@ -17,6 +24,13 @@ func _on_resized() -> void:
     $MarginContainer.rect_size = rect_size
     $MarginContainer/CenterContainer/PanelContainer.rect_min_size = \
         rect_size * (1.0 - MARGIN_RATIO * 2)
+
+
+func _get_game_over_message() -> String:
+    var index := min(
+        floor(randf() * GAME_OVER_MESSAGES.size()),
+        GAME_OVER_MESSAGES.size() - 1)
+    return GAME_OVER_MESSAGES[index]
 
 
 func _on_zaven_pressed():
